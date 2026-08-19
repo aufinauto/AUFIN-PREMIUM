5a4d146098e1d7501e03f057ab2eb76ed5414120
aufinauto <info.aufisro@gmail.com>
2026-08-19 10:13:42 +0200

    new

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -111,7 +111,7 @@ export default function Footer() {
           <Link
             href="/admin"
             aria-label="Administrace"
-            className="shrink-0 px-1 text-white/25 transition-colors hover:text-white/60"
+            className="shrink-0 px-1 text-base text-white/40 transition-colors hover:text-white/80"
           >
             ●
           </Link>
